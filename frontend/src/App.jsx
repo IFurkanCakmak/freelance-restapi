9d66840c7ef488f7491f3c0853405272cbc0ff01
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-03-29 09:54:46 +0300

    page routers added with Outlet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,81 @@
-import React from "react"
-import { Navbar } from "./components/navbar/Navbar"
-function App() {
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Jobs from "./pages/jobs/Jobs";
+import Job from "./pages/job/Job";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Add from "./pages/add/Add";
+import Orders from "./pages/orders/Orders";
+import Messages from "./pages/messages/Messages";
+import Message from "./pages/message/Message";
+import MyJobs from "./pages/myJobs/MyJobs";
+import './app.scss'
 
-  return (
-    <div>
-      <Navbar/>
-    </div>
-  )
+function App() {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/jobs",
+          element: <Jobs />,
+        },
+        {
+          path: "/myJobs",
+          element: <MyJobs />,
+        },
+        {
+          path: "/orders",
+          element: <Orders />,
+        },
+        {
+          path: "/messages",
+          element: <Messages />,
+        },
+        {
+          path: "/message/:id",
+          element: <Message />,
+        },
+        {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/job/:id",
+          element: <Job />,
+        },
+        
+      ],
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
