0a02cedba1b563bfc2d5f8137c9f0e80fe7ac4c9
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-04-03 04:13:29 +0300

    added category cards

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './Home.scss'
-import { Main } from '../../components/main/Main';
+import { Main } from '../../components/main/Main'
+import Slides from '../../components/slides/Slides';
+import { cards } from '../../data'
+import CatCard from '../../components/catCard/CatCard'
+import DiscoverProducts from '../../components/discoverProducts/DiscoverProducts';
+
 
 const Home = () => {
   return (
     <div className='home'>
       <Main/>
+      <DiscoverProducts/>
+      <Slides slidesToShow={5} arrowsScroll={1}>
+        {cards.map(card=>(
+          <CatCard key={card.id} item={card}/>
+        ))}
+      </Slides>
     </div>
   )
 };
