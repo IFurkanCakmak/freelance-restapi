0f3ca9408043cc6bd121ca870cadbf022ce3d6eb
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-04-04 03:47:01 +0300

    added

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -2,7 +2,74 @@ import React from 'react'
 import "./Footer.scss"
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='container'>
+        <div className='links'>
+          <div className='link'>
+            <h4>Categories</h4>
+            <a href='/'>
+              <p>Link1</p>
+            </a>
+             <a href='/'>
+              <p>Link1</p>
+            </a>
+             <a href='/'>
+              <p>Link1</p>
+            </a>
+          </div>
+          <div className='link'>
+            <h4>Categories</h4>
+            <a href='/'>
+              <p>Link1</p>
+            </a>
+             <a href='/'>
+              <p>Link1</p>
+            </a>
+             <a href='/'>
+              <p>Link1</p>
+            </a>
+          </div>
+          <div className='link'>
+            <h4>Categories</h4>
+            <a href='/'>
+              <p>Link1</p>
+            </a>
+          </div>
+          <div className='link'>
+            <h4>Categories</h4>
+            <a href='/'>
+              <p>Link1</p>
+            </a>
+             <a href='/'>
+              <p>Link1</p>
+            </a>
+             <a href='/'>
+              <p>Link1</p>
+            </a>
+            <a href='/'>
+              <p>Link1</p>
+            </a>
+          </div>
+          <div className='link'>
+            <img src='/images/logo.png' alt=''/>
+          </div>
+        </div>
+        <hr></hr>
+        <div className='bottom'>
+          <div className='copyright'>
+            <p>
+              @{new Date().getFullYear()} Izzet Furkan Cakmak
+            </p>
+          </div>
+          <div className='bottomLink'>
+            <a href='/terms'><div><p>Source Code</p></div></a>
+            <a href='/terms'><div><p>Source Code</p></div></a>
+            <a href='/terms'><div><p>Source Code</p></div></a>
+            <a href='/terms'><div><p>Source Code</p></div></a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ frontend/src/components/jobCard/JobCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./JobCard.scss"
+import {Link} from 'react-router-dom'
+
+const JobCard = ({item}) => {
+  return (
+    <Link to="/job/123">
+    <div className='jobCard'>
+        <img src={item.img} alt=''/>
+        <div className='info'>
+        <div className='user'>
+        <img src={item.pp} alt=''/>
+        <span>{item.username}</span>
+        </div>
+        <p>{item.desc}</p>
+        <div className='star'>
+            <img src="/images/star.png" alt=''/>
+            <span>{item.star}</span>
+        </div>
+        </div>
+        <div className='details'>
+            <span>Starting at</span>
+            <h2>${item.price}</h2>
+        </div>
+    </div>
+    </Link>
+    
+  )
+}
+
+export default JobCard
