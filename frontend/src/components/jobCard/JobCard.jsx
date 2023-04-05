a642dabd3181f1442459c0a3e88f18cd96f1d0b0
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-04-05 10:21:53 +0300

    add update

--- frontend/src/components/jobCard/JobCard.jsx
+++ frontend/src/components/jobCard/JobCard.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
-import "./JobCard.scss"
-import {Link} from 'react-router-dom'
+import React from "react";
+import "./JobCard.scss";
+import { Link } from "react-router-dom";
 
-const JobCard = ({item}) => {
+const JobCard = ({ item }) => {
   return (
     <Link to="/job/123">
-    <div className='jobCard'>
-        <img src={item.img} alt=''/>
-        <div className='info'>
-        <div className='user'>
-        <img src={item.pp} alt=''/>
-        <span>{item.username}</span>
-        </div>
-        <p>{item.desc}</p>
-        <div className='star'>
-            <img src="/images/star.png" alt=''/>
+      <div className="jobCard">
+        <img src={item.img} alt="" />
+        <div className="info">
+          <div className="user">
+            <img src={item.pp} alt="" />
+            <span>{item.username}</span>
+          </div>
+          <p>{item.desc}</p>
+          <div className="star">
+            <img src="/images/star.png" alt="" />
             <span>{item.star}</span>
+          </div>
         </div>
-        </div>
-        <div className='details'>
+        <hr />
+        <div className="details">
+          <div className="price">
             <span>Starting at</span>
             <h2>${item.price}</h2>
+          </div>
         </div>
-    </div>
+      </div>
     </Link>
-    
-  )
-}
+  );
+};
 
-export default JobCard
+export default JobCard;
