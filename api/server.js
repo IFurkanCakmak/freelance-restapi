6cdbced85815be111e878e3384b456e6efe5fdb3
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-04-18 16:27:41 +0300

    added routes

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js"
+import jobRoute from "./routes/job.route.js"
+import reviewRoute from "./routes/review.route.js"
+import orderRoute from "./routes/order.route.js"
+import conversationRoute from "./routes/conversation.route.js"
+import messageRoute from "./routes/message.route.js"
 
 const app = express();
 dotenv.config();
@@ -13,6 +19,13 @@ const connect = async () => {
     console.log(error);
   }
 };
+
+app.use("/api/users" , userRoute)
+app.use("/api/messages" , messageRoute)
+app.use("/api/conversations" , conversationRoute)
+app.use("/api/orders" , orderRoute)
+app.use("/api/reviews" , reviewRoute)
+app.use("/api/jobs" , jobRoute)
 
 app.listen(8800, () => {
     connect()
