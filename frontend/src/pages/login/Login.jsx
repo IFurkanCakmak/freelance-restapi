df0392d6c9343afc6ee91ebd330402d9cab79a4d
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-04-29 05:30:43 +0300

    login in local storage

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,24 +1,24 @@
 import React, { useState } from "react";
 import "./Login.scss";
-import axios from "axios";
+import newRequest from "../../utils/newRequest";
+import {useNavigate} from "react-router-dom"
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
+  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(
-        "http://localhost:8800/api/auth/login",
-        { username, password },
-        { withCredentials: true } /* TODO THIS DOESNT WORK */
-      );
-      console.log(res.data);
+      const res = await newRequest.post("/auth/login", {username, password});
+      localStorage.setItem("currentUser", JSON.stringify(res.data)); /* can set only string to localstorage so make it string than object */
+      navigate("/")
     } catch (err) {
-      setError(err);
-      console.log(err);
+      setError(err).response.data;
+      
     }
   };
   return (
@@ -41,6 +41,7 @@ function Login() {
         />
 
         <button type="submit">Login</button>
+        {error && error}
       </form>
     </div>
   );
