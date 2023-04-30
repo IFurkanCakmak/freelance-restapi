d2b54550ec5f5a0c24efe15a95541a89e7e41a68
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-04-30 05:25:10 +0300

    register auth processes all set

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
-
 import "./Register.scss";
+import upload from "../../utils/upload.js";
+import newRequest from "../../utils/newRequest.js";
+import { useNavigate } from "react-router-dom";
+
 
 
 function Register() {
 
+  const [file , setFile] = useState(null)
   const [user, setUser] = useState({
     username: "",
     email: "",
@@ -14,15 +18,45 @@ function Register() {
     isSeller: false,
     desc: "",
   });
+
+  const navigate =useNavigate()
   
+
 
   const handleChange = (e) =>{
 
-  }
+    setUser(prev => {
+      return{...prev, [e.target.name] : e.target.value}; /* TODO check what it is */
+    })
+  };
+
+  const handleSeller = (e) =>{
+
+    setUser(prev => {
+      return{...prev, isSeller : e.target.checked}; /* TODO check what it is */
+    })
+  };
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+
+    const url = await upload(file)
+   try {
+    await newRequest.post("/auth/register", {
+      ...user,
+      img:url
+    });
+    navigate("/")
+   } catch (err) {
+
+    console.log(err)
+    
+   }
+  };
   
   return (
     <div className="register">
-      <form >
+      <form onSubmit={handleSubmit}>
         <div className="left">
           <h1>Create a new account</h1>
           <label htmlFor="">Username</label>
@@ -43,7 +77,7 @@ function Register() {
           <label htmlFor="">Password</label>
           <input name="password" type="password" onChange={handleChange} />
           <label htmlFor="">Profile Picture</label>
-          <input type="file"/>
+          <input type="file" onChange={(e)=>setFile(e.target.files[0])}/>
           <label htmlFor="">Country</label>
           <input
             name="country"
@@ -58,7 +92,7 @@ function Register() {
           <div className="toggle">
             <label htmlFor="">Activate the seller account</label>
             <label className="switch">
-              <input type="checkbox" />
+              <input type="checkbox" onChange={handleSeller}/>
               <span className="slider round"></span>
             </label>
           </div>
