ef83e069f1b9ab6e3a2fa2bab2ea1c3acd22108b
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-04-30 05:24:20 +0300

    update

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -24,7 +24,7 @@ export const register = async (req, res, next) => {
 export const login = async (req, res, next) => {
   try {
     const user = await User.findOne({
-      username: req.body.username,
+      username: req.body.username
     }); /* find user by find function in mongo */
 
     if (!user) return next(createError(404, "User not found"));
