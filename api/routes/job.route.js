1608e8fab097c9edefe49654efbbdbc46396647f
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-05-01 04:54:47 +0300

    job routes added

--- api/routes/job.route.js
+++ api/routes/job.route.js
@@ -1,9 +1,19 @@
 import express from "express";
-
+import {
+    createJob,
+    deleteJob,
+    getJob,
+    getJobs,
+}
+from "../controllers/job.controller.js"
+import {verifyToken} from "../middleware/jwt.js" 
 
 const router = express.Router()
 
-router.get("/register" , )
-router.get("/login" , ) 
+router.post("/" , verifyToken, createJob )
+router.delete("/:id" , verifyToken, deleteJob )
+router.get("/single/:id" , verifyToken, getJob )
+router.get("/" , verifyToken, getJobs )
+
 
 export default router;
