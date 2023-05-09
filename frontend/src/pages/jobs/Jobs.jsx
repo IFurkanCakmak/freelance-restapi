d18ec36a6a11bb296abbe3cbc5a84f9a1fb792d7
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-05-09 03:53:07 +0300

    fetch and sort all data

--- frontend/src/pages/jobs/Jobs.jsx
+++ frontend/src/pages/jobs/Jobs.jsx
@@ -1,54 +1,90 @@
-import React, { useState } from 'react'
-import './Jobs.scss'
-import { jobs } from '../../data';
-import JobCard from "../../components/jobCard/JobCard"
-
- 
-
- const Jobs = () => {
-
-  const [sort, setSort]= useState("sales");
-  const [open , setOpen] =useState(false)
+import React, { useEffect, useRef, useState } from "react";
+import "./Jobs.scss";
+import JobCard from "../../components/jobCard/JobCard";
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest.js";
+import { useLocation } from "react-router-dom";
 
 
-  const reSort=(type)=>{
-    setSort(type)
-    setOpen(false)
+const Jobs = () => {
+  const [sort, setSort] = useState("sales");
+  const [open, setOpen] = useState(false);
+  const minRef = useRef();
+  const maxRef = useRef();
+
+  const { search } = useLocation();
+
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: ["jobs"],
+    queryFn: () =>
+      newRequest
+        .get(
+          `/jobs${search}&min=${minRef.current.value}&max=${maxRef.current.value}&sort=${sort}`
+        )
+        .then((res) => {
+          return res.data;
+        }),
+  });
+
+  console.log(data);
+
+  const reSort = (type) => {
+    setSort(type);
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    refetch();
+  },[sort]); /* refetch jobs whenever this sort changes */
+
+  const apply = () =>{
+    refetch()
   }
 
   return (
-    <div className='jobs'>
-      <div className='container'>
-        <span className='littlecat'>Jobook &gt; Web Design</span>
+    <div className="jobs">
+      <div className="container">
+        <span className="littlecat">Jobook &gt; Web Design</span>
         <h1>Web Developers</h1>
         <p>Explore the freelancers</p>
-        <div className='menu'>
-          <div className='left'>
+        <div className="menu">
+          <div className="left">
             <span>Budget : </span>
-            <input type='text' placeholder='min'/>
-            <input type='text' placeholder='max'/>
-            <button>Apply</button>
+            <input ref={minRef} type="number" placeholder="min" />
+            <input ref={maxRef} type="number" placeholder="max" />
+            <button onClick={apply}>Apply</button>
           </div>
-          <div className='right'>
-            <span className='sortBy'>Sort : </span>
-            <span className='sortType'>{sort=== "sales" ? "Best Selling" : "Latest"}</span>
-            <img src='./images/down.png' alt='' onClick={()=>setOpen(!open)}/>
-            {open &&(
-              <div className='rightMenu'>
-              {sort ==="sales" ?<span onClick={()=>reSort("createdAt")}>Latest</span>
-              :<span onClick={()=>reSort("sales")}>Best Selling</span>}
-            </div>
+          <div className="right">
+            <span className="sortBy">Sort : </span>
+            <span className="sortType">
+              {sort === "sales" ? "Best Selling" : "Latest"}
+            </span>
+            <img
+              src="./images/down.png"
+              alt=""
+              onClick={() => setOpen(!open)}
+            />
+            {open && (
+              <div className="rightMenu">
+                {sort === "sales" ? (
+                  <span onClick={() => reSort("createdAt")}>Latest</span>
+                ) : (
+                  <span onClick={() => reSort("sales")}>Best Selling</span>
+                )}
+                <span onClick={() => reSort("sales")}>Popular</span>
+              </div>
             )}
-            
           </div>
         </div>
-        <div className='cards'>
-          {jobs.map(job=>(
-            <JobCard key={job.id} item={job}/>
-          ))}
+        <div className="cards">
+          {isLoading
+            ? "loading"
+            : error
+            ? "Something went wrong"
+            : data.map((job) => <JobCard key={job._id} item={job} />)}
         </div>
       </div>
     </div>
-  )
-}
-export default Jobs
+  );
+};
+export default Jobs;
