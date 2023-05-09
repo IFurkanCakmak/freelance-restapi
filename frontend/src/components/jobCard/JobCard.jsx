b51177c16960246bab59deabcbc2a265f5dea514
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-05-09 03:52:06 +0300

    get user data and star rating in jobcard

--- frontend/src/components/jobCard/JobCard.jsx
+++ frontend/src/components/jobCard/JobCard.jsx
@@ -1,26 +1,42 @@
 import React from "react";
 import "./JobCard.scss";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
 
 const JobCard = ({ item }) => {
+  const { isLoading, error, data } = useQuery({
+    queryKey: [item.userId],
+    queryFn: () =>
+      newRequest.get(`/users/${item.userId}`).then((res) => {
+        return res.data;
+      }),
+  });
+
   return (
     <Link to="/job/123" className="link">
       <div className="jobCard">
-        <img src={item.img} alt="" />
+        <img src={item.cover} alt="" />
         <div className="info">
-          <div className="user">
-            <img src={item.pp} alt="" />
-            <span>{item.username}</span>
-          </div>
+          {isLoading ? (
+            "loading"
+          ) : error ? (
+            "sometghin went wrong "
+          ) : (
+            <div className="user">
+              <img src={data.img || "/images/noimage.jpg"} alt="" />
+              <span>{data.username}</span>
+            </div>
+          )}
           <p>{item.desc}</p>
           <div className="star">
             <img src="/images/star.png" alt="" />
-            <span>{item.star}</span>
+            <span>{!isNaN(item.totalStars / item.starNumber)  &&  Math.round(item.totalStars / item.starNumber)}/5</span>
           </div>
         </div>
         <hr />
         <div className="details">
-        <img src="/images/heart.png" alt="" />
+          <img src="/images/heart.png" alt="" />
           <div className="price">
             <span>Starting at</span>
             <h2>${item.price}</h2>
