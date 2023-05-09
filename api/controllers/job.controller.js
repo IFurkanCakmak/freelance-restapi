a2a98b30a231d19349ff0f9d0f5091b3f5bd3168
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-05-09 03:51:05 +0300

    sort query filter added

--- api/controllers/job.controller.js
+++ api/controllers/job.controller.js
@@ -20,44 +20,43 @@ export const createJob = async (req, res, next) => {
 export const deleteJob = async (req, res, next) => {
   try {
     /* first find the job and check if the userid equals owner that job can be deleted*/
-
     const job = await Job.findById(req.params.id);
-
     if (job.userId !== req.userId)
       return next(createError(403, "You can delete only our job"));
 
-      await Job.findByIdAndDelete(req.params.id);
-      res.status(200).send("Job has been deleted");
+    await Job.findByIdAndDelete(req.params.id);
+    res.status(200).send("Job has been deleted!");
   } catch (err) {
     next(err);
   }
 };
 export const getJob = async (req, res, next) => {
-    try {
-        const job = await Job.findById(req.params.id);
-        if(!job) next(createError(404, "Job not found"))
-        res.status(200).send(job)
-    } catch (err) {
-        next(err);
-    }
+  try {
+    const Job = await Job.findById(req.params.id);
+    if (!job) next(createError(404, "Job not found!"));
+    res.status(200).send(job);
+  } catch (err) {
+    next(err);
+  }
 };
+
 export const getJobs = async (req, res, next) => {
-
-    const q = req.query;
-    const filters= {
-        ...(q.userId && {userId:q.userId}),
-        ...(q.cat && {cat:q.cat}),
-        ...((q.min || q.max) && {
-            price: {...(q.min && {$gt: q.min}), ...(q.max && { $lt: q.max})},
-        }),
-
-        ...(q.search && {title:{$regex: q.search, $options: "i"}}),
-    };
-    try {
-        const jobs = await Job.find(filters)
-        res.status(200).send(jobs)
-    } catch (err) {
-        next(err)
-        
-    }
+  const q = req.query;
+  const filters = {
+    ...(q.userId && { userId: q.userId }),
+    ...(q.cat && { cat: q.cat }),
+    ...((q.min || q.max) && {
+      price: {
+        ...(q.min && { $gt: q.min }),
+        ...(q.max && { $lt: q.max }),
+      },
+    }),
+    ...(q.search && { title: { $regex: q.search, $options: "i" } }),
+  };
+  try {
+    const jobs = await Job.find(filters).sort({ [q.sort]: -1 });
+    res.status(200).send(jobs);
+  } catch (err) {
+    next(err);
+  }
 };
