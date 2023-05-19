7ece69bd34745ad7cd4488d1500a3efbdba0b864
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-05-19 04:02:33 +0300

    orders functions created

--- frontend/src/pages/orders/Orders.jsx
+++ frontend/src/pages/orders/Orders.jsx
@@ -1,100 +1,56 @@
-import React from 'react'
-import {Link} from "react-router-dom"
-import "./Orders.scss"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Orders.scss";
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
 
 const Orders = () => {
-  
-  const currentUser = {
-    id:1,
-    username:"Mada Faka",
-    isSeller:true,
-  };
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["orders"],
+    queryFn: () =>
+      newRequest.get(`/orders`).then((res) => {
+        return res.data;
+      }),
+  });
+
+  /* the function above is the reusable react query function for fetch data */
   return (
-    <div className='orders'>
-      <div className='container'>
-        <div className='title'>
-          <h1>Orders</h1>
+    <div className="orders">
+      {isLoading ? (
+        "loading"
+      ) : error ? (
+        "Something went wrong"
+      ) : (
+        <div className="container">
+          <div className="title">
+            <h1>Orders</h1>
+          </div>
+          <table>
+            <tr>
+              <th>Image</th>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Contact</th>
+            </tr>
+            {data.map((order) => (
+              <tr key={order._id}>
+                <td>
+                  <img className="image" src={order.img} alt="" />
+                </td>
+                <td>{order.title}</td>
+                <td>{order.price}</td>
+                <td>
+                  <img className="contact" src="/images/contact.png" alt="" />
+                </td>
+              </tr>
+            ))}
+          </table>
         </div>
-        <table>
-          <tr>
-            <th>Image</th>
-            <th>Title</th>
-            <th>Price</th>
-             {/*if current user seller displays clinet if not displays seller  */}
-            <th>{currentUser?.isSeller ? "Client" :"Seller"}</th>
-            <th>Contact</th>
-          </tr>
-          <tr>
-            <td>
-              <img className='image' src='/images/myjob.png' alt=''/>
-            </td>
-            <td>Job1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img className='contact' src='/images/contact.png' alt=''/>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className='image' src='/images/myjob.png' alt=''/>
-            </td>
-            <td>Job1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img className='contact' src='/images/contact.png' alt=''/>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className='image' src='/images/myjob.png' alt=''/>
-            </td>
-            <td>Job1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img className='contact' src='/images/contact.png' alt=''/>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className='image' src='/images/myjob.png' alt=''/>
-            </td>
-            <td>Job1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img className='contact' src='/images/contact.png' alt=''/>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className='image' src='/images/myjob.png' alt=''/>
-            </td>
-            <td>Job1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img className='contact' src='/images/contact.png' alt=''/>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className='image' src='/images/myjob.png' alt=''/>
-            </td>
-            <td>Job1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img className='contact' src='/images/contact.png' alt=''/>
-            </td>
-          </tr>
-        </table>
-      </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Orders
+export default Orders;
