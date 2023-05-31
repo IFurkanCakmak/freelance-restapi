2782d1b082d0db3050d1ab0b4efe3f19a35e3afe
Furkan Çakmak <73801482+IFurkanCakmak@users.noreply.github.com>
2023-05-31 03:09:16 +0300

    selfjobs page created

--- frontend/src/pages/myJobs/MyJobs.jsx
+++ frontend/src/pages/myJobs/MyJobs.jsx
@@ -1,95 +1,86 @@
-import React from 'react'
-import {Link} from "react-router-dom"
-import "./MyJobs.scss"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./MyJobs.scss";
+import getCurrentUser from "../../utils/getCurrentUser";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
 
 const MyJobs = () => {
-  return (
-    <div className='myJobs'>
-      <div className='container'>
-        <div className='title'>
-          <h1>Jobs</h1>
-          <Link to="/add">
-            <button>Add new job</button>
-          </Link>
-        </div>
-        <table>
-          <tr>
-            <th>Image</th>
-            <th>Title</th>
-            <th>Price</th>
-            <th>Sales</th>
-            <th>Action</th>
-          </tr>
-          <tr>
-            <td>
-              <img className='image' src='/images/myjob.png' alt=''/>
-            </td>
-            <td>Job1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img className='delete' src='/images/delete.png' alt=''/>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className='image' src='/images/myjob.png' alt=''/>
-            </td>
-            <td>Job1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img className='delete' src='/images/delete.png' alt=''/>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className='image' src='/images/myjob.png' alt=''/>
-            </td>
-            <td>Job1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img className='delete' src='/images/delete.png' alt=''/>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className='image' src='/images/myjob.png' alt=''/>
-            </td>
-            <td>Job1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img className='delete' src='/images/delete.png' alt=''/>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className='image' src='/images/myjob.png' alt=''/>
-            </td>
-            <td>Job1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img className='delete' src='/images/delete.png' alt=''/>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className='image' src='/images/myjob.png' alt=''/>
-            </td>
-            <td>Job1</td>
-            <td>88</td>
-            <td>123</td>
-            <td>
-              <img className='delete' src='/images/delete.png' alt=''/>
-            </td>
-          </tr>
-        </table>
-      </div>
-    </div>
-  )
-}
+  const currentUser = getCurrentUser();
 
-export default MyJobs
+  const queryClient = useQueryClient();
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["myJobs"],
+    queryFn: () =>
+      newRequest.get(`/jobs?userId=${currentUser._id}`).then((res) => {
+        return res.data;
+      }),
+  });
+
+  const mutation = useMutation({
+    mutationFn: (id) => {
+      return newRequest.delete(`/jobs/${id}`);
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries(["myJobs"]);
+    },
+  });
+
+  const handleDelete = (id) =>  {
+    mutation.mutate(id);
+  }
+
+  return (
+    <div className="myJobs">
+      {isLoading ? (
+        "Loading"
+      ) : error ? (
+        "Something went wrong"
+      ) : (
+        <div className="container">
+          <div className="title">
+            <h1>İlanlar</h1>
+            {currentUser.isSeller && (
+              <Link to="/add">
+                <button>Yeni ilan ekle</button>
+              </Link>
+            )}
+          </div>
+          <table>
+            <tr>
+              <th>Kapak resmi</th>
+              <th>Başlık</th>
+              <th>Fiyat</th>
+              <th>Sipariş sayısı</th>
+              <th>İşlem</th>
+            </tr>
+          {data.map((job)=>(
+
+            <tr key={job._id}>
+              <td>
+                <img className="image" src={job.cover} alt="" />
+              </td>
+              <td>{job.title}</td>
+              <td>{job.price}</td>
+              <td>{job.sales}</td>
+              <td>
+                <img
+                  className="delete"
+                  src="/images/delete.png"
+                  alt=""
+                  onClick={()=>handleDelete(job._id)}
+                />
+              </td>
+            </tr>
+
+          )) }
+          </table>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MyJobs;
